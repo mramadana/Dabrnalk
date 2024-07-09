05b49c892acbeb5ad18e7174f2bfee8408f30132
mramadan5080@gmail.com <mramadan5080@gmail.com>
2024-07-09 10:21:40 +0300

    finished_without_cupon_2

---
+++ error.vue
@@ -0,0 +1,268 @@
+<template>
+    <div class="not-found">
+        <div class="content">
+
+            <div id="background"></div>
+            <div class="top">
+                <h1>404</h1>
+                <h3>page not found</h3>
+            </div>
+            <div class="container_face">
+                <div class="ghost-copy">
+                    <div class="one"></div>
+                    <div class="two"></div>
+                    <div class="three"></div>
+                    <div class="four"></div>
+                </div>
+                <div class="ghost">
+                    <div class="face">
+                        <div class="eye"></div>
+                        <div class="eye-right"></div>
+                        <div class="mouth"></div>
+                    </div>
+                </div>
+                <div class="shadow"></div>
+            </div>
+            <div class="bottom">
+                <div class="buttons">
+                    <router-link to="/" class="custom-btn sm up">Home</router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+
+<style lang="scss" scoped>
+
+    $white: #EDEDED;
+    $gray: #BFC0C0;
+    $dark: #585959;
+    $light: #D3DEEA;
+
+@import url('https://fonts.googleapis.com/css?family=Abril+Fatface|Lato');
+
+$big: 'Abril Fatface', serif;
+$body: 'Lato', sans-serif;
+
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+.with-pt {
+    padding-top: 0 !important;
+    padding-bottom: 0 !important;
+}
+
+.not-found {
+    background: #202020;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    min-height: 575px;
+    position: fixed;
+    left: 0;
+    top: 0;
+}
+
+.top {
+    margin-bottom: 50px;
+}
+
+.container_face {
+    margin: 0 auto;
+    position: relative;
+    width: 250px;
+    height: 250px;
+    margin-top: -40px;
+}
+
+.ghost {
+    width: 50%;
+    height: 53%;
+    left: 25%;
+    top: 10%;
+    position: absolute;
+    border-radius: 50% 50% 0 0;
+    background: $white;
+    border: 1px solid $gray;
+    border-bottom: none;
+    animation: float 2s ease-out infinite;
+}
+
+.ghost-copy {
+    width: 50%;
+    height: 53%;
+    left: 25%;
+    top: 10%;
+    position: absolute;
+    border-radius: 50% 50% 0 0;
+    background: $white;
+    border: 1px solid $gray;
+    border-bottom: none;
+    animation: float 2s ease-out infinite;
+    z-index: 0;
+}
+
+
+.face {
+    position: absolute;
+    width: 100%;
+    height: 60%;
+    top: 20%;
+}
+
+.eye,
+.eye-right {
+    position: absolute;
+    background: $dark;
+    width: 13px;
+    height: 13px;
+    border-radius: 50%;
+    top: 40%;
+}
+
+.eye {
+    left: 25%;
+}
+
+.eye-right {
+    right: 25%;
+}
+
+.mouth {
+    position: absolute;
+    top: 50%;
+    left: 45%;
+    width: 10px;
+    height: 10px;
+    border: 3px solid;
+    border-radius: 50%;
+    border-color: transparent $dark $dark transparent;
+    transform: rotate(45deg);
+}
+
+.one,
+.two,
+.three,
+.four {
+    position: absolute;
+    background: $white;
+    top: 85%;
+    width: 25%;
+    height: 23%;
+    border: 1px solid $gray;
+    z-index: 0;
+}
+
+.one {
+    border-radius: 0 0 100% 30%;
+    left: -1px;
+}
+
+.two {
+    left: 23%;
+    border-radius: 0 0 50% 50%;
+}
+
+.three {
+    left: 50%;
+    border-radius: 0 0 50% 50%;
+}
+
+.four {
+    left: 74.5%;
+    border-radius: 0 0 30% 100%;
+}
+
+.shadow {
+    position: absolute;
+    width: 30%;
+    height: 7%;
+    background: $gray;
+    left: 35%;
+    top: 80%;
+    border-radius: 50%;
+    animation: scale 2s infinite;
+}
+
+@keyframes scale {
+    0% {
+        transform: scale(1);
+    }
+
+    50% {
+        transform: scale(1.1);
+    }
+
+    100% {
+        transform: scale(1);
+    }
+}
+
+@keyframes float {
+    50% {
+        transform: translateY(15px);
+    }
+}
+
+.bottom {
+    margin-top: 10px;
+}
+
+/*text styling*/
+h1 {
+    font-family: $big;
+    color: $white;
+    text-align: center;
+    font-size: 9em;
+    margin: 0;
+    text-shadow: -1px 0 $gray, 0 1px $gray, 1px 0 $gray, 0 -1px $gray;
+
+    @media screen and (max-width: 500px) {
+        font-size: 5rem;
+    }
+}
+
+h3 {
+    font-family: $body;
+    font-size: 2em;
+    text-transform: uppercase;
+    text-align: center;
+    color: $gray;
+    margin-top: 20px;
+    font-weight: 900;
+
+    @media screen and (max-width: 500px) {
+        font-size: 1.6rem;
+        margin-top: 10px;
+    }
+}
+
+p {
+    text-align: center;
+    font-family: $body;
+    color: $dark;
+    font-size: .6em;
+    margin-top: -20px;
+    text-transform: uppercase;
+}
+
+.search {
+    text-align: center;
+}
+
+.buttons {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 10px;
+
+}</style>
